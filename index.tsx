9c9d7fda62335094061552c8e29dcaa2a5925865
BLIVMEDIALLP <a38169794@gmail.com>
2026-02-21 05:54:16 +0530

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1608,3 +1608,8 @@ export default function App() {
     </div>
   );
 }
+const container = document.getElementById('root');
+if (container) {
+  const root = createRoot(container);
+  root.render(<App />);
+}
